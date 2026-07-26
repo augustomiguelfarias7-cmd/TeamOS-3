d95b08b03fb039fe5232bc3eb2e578ee5f0723d5
augustomiguelfarias7-cmd <augustomiguelfarias7@gmail.com>
2026-07-26 01:53:00 -0300

    Adiciona GalleryActivity.kt

---
+++ launcher_apps/apps/gallery/GalleryActivity.kt
@@ -0,0 +1,20 @@
+package com.teamos.gallery
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.teamos.launcher.R
+
+/**
+ * App Galeria nativo - TeamOS 3.0
+ * Visual: One UI 8 + Android 16 Material 3 Expressive
+ */
+class GalleryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+
+        // Grid de imagens + visualização em tela cheia
+        // Integração com a loja de papéis de parede
+    }
+}
